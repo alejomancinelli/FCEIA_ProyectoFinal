b8d307f986ac0c6747380a4f3d7bdf2673c95215
Alejo Mancinelli <alejomancinelli98@gmail.com>
2024-07-17 19:17:41 -0300

    WiFi connection config

--- BL0940_ESP8266/BL0940_ESP8266.ino
+++ BL0940_ESP8266/BL0940_ESP8266.ino
@@ -19,12 +19,6 @@ String pass;
 String ip;
 String gateway;
 
-// File paths to save input values permanently
-const char* ssidPath = "/ssid.txt";
-const char* passPath = "/pass.txt";
-const char* ipPath = "/ip.txt";
-const char* gatewayPath = "/gateway.txt";
-
 IPAddress localIP;
 //IPAddress localIP(192, 168, 1, 200); // hardcoded
 
@@ -40,6 +34,17 @@ const long interval = 10000;  // interval to wait for Wi-Fi connection (millisec
 String ledState;
 
 bool restart = false;
+bool ap_running = false;
+
+// -------------------------------------------------------------- //
+// ------------------------ [ LittleFS ] ------------------------ //
+// -------------------------------------------------------------- //
+
+// File paths to save input values permanently
+const char* ssidPath = "/ssid.txt";
+const char* passPath = "/pass.txt";
+const char* ipPath = "/ip.txt";
+const char* gatewayPath = "/gateway.txt";
 
 // Initialize LittleFS
 void initFS() {
@@ -50,7 +55,6 @@ void initFS() {
     Serial.println("LittleFS mounted successfully");
   }
 }
-
 
 // Read File from LittleFS
 String readFile(fs::FS &fs, const char * path){
@@ -88,11 +92,24 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
   file.close();
 }
 
+// -------------------------------------------------------------- //
+// -------------------------- [ Wifi ] -------------------------- //
+// -------------------------------------------------------------- //
+
+const int WIFI_RESET_BUTTON = 14;
+
+enum WIFI_ERROR_CODES {
+  SUCCESFULLY_CONNECTED = 0,
+  UNDEFINED_SSID_OR_IP = 1,
+  STA_FAILED = 2,
+  FAILED_TO_CONNECT = 3,
+};
+
 // Initialize WiFi
-bool initWiFi() {
+int initWiFi() {
   if(ssid=="" || ip==""){
     Serial.println("Undefined SSID or IP address.");
-    return false;
+    return UNDEFINED_SSID_OR_IP;
   }
 
   WiFi.mode(WIFI_STA);
@@ -101,25 +118,95 @@ bool initWiFi() {
 
   if (!WiFi.config(localIP, localGateway, subnet)){
     Serial.println("STA Failed to configure");
-    return false;
+    return STA_FAILED;
   }
   WiFi.begin(ssid.c_str(), pass.c_str());
 
-  // TODO: Loop para reintentar la conexión de forma indefinida
+  // TODO: Podría ser una especide for y que lo haga un par de veces?
   Serial.println("Connecting to WiFi...");
-  delay(20000);
-  if(WiFi.status() != WL_CONNECTED) {
-    Serial.println("Failed to connect.");
-    return false;
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println("Failed to connect. Retrying WiFi connection in 5 seconds...");
+    delay(5000);
   }
 
+  Serial.println("Wifi connected!.");
   Serial.println(WiFi.localIP());
-  return true;
+  return SUCCESFULLY_CONNECTED;
 }
 
+void initAP(){
+  // Connect to Wi-Fi network with SSID and password
+  Serial.println("Setting AP (Access Point)");
+  // NULL sets an open Access Point
+  WiFi.softAP("ESP-WIFI-MANAGER", NULL);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP); 
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/wifimanager.html", "text/html");
+  });
+  
+  server.serveStatic("/", LittleFS, "/");
+  
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    int params = request->params();
+    for(int i=0; i<params; i++) {
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST ssid value
+        if (p->name() == PARAM_INPUT_1) {
+          ssid = p->value().c_str();
+          Serial.print("SSID set to: ");
+          Serial.println(ssid);
+          // Write file to save value
+          writeFile(LittleFS, ssidPath, ssid.c_str());
+        }
+        // HTTP POST pass value
+        if (p->name() == PARAM_INPUT_2) {
+          pass = p->value().c_str();
+          Serial.print("Password set to: ");
+          Serial.println(pass);
+          // Write file to save value
+          writeFile(LittleFS, passPath, pass.c_str());
+        }
+        // HTTP POST ip value
+        if (p->name() == PARAM_INPUT_3) {
+          ip = p->value().c_str();
+          Serial.print("IP Address set to: ");
+          Serial.println(ip);
+          // Write file to save value
+          writeFile(LittleFS, ipPath, ip.c_str());
+        }
+        // HTTP POST gateway value
+        if (p->name() == PARAM_INPUT_4) {
+          gateway = p->value().c_str();
+          Serial.print("Gateway set to: ");
+          Serial.println(gateway);
+          // Write file to save value
+          writeFile(LittleFS, gatewayPath, gateway.c_str());
+        }
+      }
+    }
+    restart = true;
+    request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+  });
+  server.begin();
+  ap_running = true;
+}
+
+// -------------------------------------------------------------- //
+// -------------------------- [ Setup ] ------------------------- //
+// -------------------------------------------------------------- //
+
 void setup() {
+  
   // Serial port for debugging purposes
   Serial.begin(9600);
+  
+  pinMode(WIFI_RESET_BUTTON, INPUT_PULLUP);
 
   initFS();
 
@@ -133,71 +220,22 @@ void setup() {
   Serial.println(ip);
   Serial.println(gateway);
 
-  if(!initWiFi()) {
-    // Connect to Wi-Fi network with SSID and password
-    Serial.println("Setting AP (Access Point)");
-    // NULL sets an open Access Point
-    WiFi.softAP("ESP-WIFI-MANAGER", NULL);
-
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP); 
-
-    // Web Server Root URL
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(LittleFS, "/wifimanager.html", "text/html");
-    });
-    
-    server.serveStatic("/", LittleFS, "/");
-    
-    server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
-      int params = request->params();
-      for(int i=0; i<params; i++) {
-        AsyncWebParameter* p = request->getParam(i);
-        if(p->isPost()){
-          // HTTP POST ssid value
-          if (p->name() == PARAM_INPUT_1) {
-            ssid = p->value().c_str();
-            Serial.print("SSID set to: ");
-            Serial.println(ssid);
-            // Write file to save value
-            writeFile(LittleFS, ssidPath, ssid.c_str());
-          }
-          // HTTP POST pass value
-          if (p->name() == PARAM_INPUT_2) {
-            pass = p->value().c_str();
-            Serial.print("Password set to: ");
-            Serial.println(pass);
-            // Write file to save value
-            writeFile(LittleFS, passPath, pass.c_str());
-          }
-          // HTTP POST ip value
-          if (p->name() == PARAM_INPUT_3) {
-            ip = p->value().c_str();
-            Serial.print("IP Address set to: ");
-            Serial.println(ip);
-            // Write file to save value
-            writeFile(LittleFS, ipPath, ip.c_str());
-          }
-          // HTTP POST gateway value
-          if (p->name() == PARAM_INPUT_4) {
-            gateway = p->value().c_str();
-            Serial.print("Gateway set to: ");
-            Serial.println(gateway);
-            // Write file to save value
-            writeFile(LittleFS, gatewayPath, gateway.c_str());
-          }
-          //Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-        }
-      }
-      restart = true;
-      request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
-    });
-    server.begin();
+  if(initWiFi() == (UNDEFINED_SSID_OR_IP || STA_FAILED)) {
+    initAP();
   }
 }
 
+// -------------------------------------------------------------- //
+// -------------------------- [ Loop ] -------------------------- //
+// -------------------------------------------------------------- //
+
 void loop() {
+  if (digitalRead(WIFI_RESET_BUTTON) && !ap_running){
+    Serial.print("Button: ");
+    Serial.println(digitalRead(WIFI_RESET_BUTTON));
+    initAP();
+  }
+
   if (restart){
     delay(5000);
     ESP.restart();
